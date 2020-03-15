6449ef233608c865fceb9e72872ab5fed27387ec
Julien Blondeau <julienblondeau@live.fr>
2020-03-15 15:47:20 +0100

    create course mongoose model

---
+++ models/course.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const courseSchema = new Schema({
+  creator: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 4,
+    maxlength: 40
+  },
+  language: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 24
+  },
+  countryFlag: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 2
+  },
+  learningFrom: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 24
+  },
+  vocabulary: Array,
+  quizzes: Array
+
+});
+
+module.exports = mongoose.model('Course', courseSchema);
