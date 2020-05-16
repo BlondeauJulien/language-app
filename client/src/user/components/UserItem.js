71453fc069b7a71aebdb042f595f82a85762991c
Julien Blondeau <julienblondeau@live.fr>
2020-05-16 18:37:43 +0200

    ban user from users list

--- client/src/user/components/UserItem.js
+++ client/src/user/components/UserItem.js
@@ -1,24 +1,51 @@
-import React from 'react';
+import React, { useContext, Fragment, useState } from 'react';
 
 import Button from '../../shared/components/FormElements/Button';
+import PasswordForm from '../../shared/components/FormElements/PasswordForm';
+import AuthContext from '../../context/auth/authContext';
 
 import './UserItem.css';
 
 const UserItem = props => {
   const { user } = props;
+  const authContext = useContext(AuthContext);
+
+  const { token, banUser } = authContext;
+  const [openPassWordForm, setOpenPassWordForm ] = useState(false);
+  const [passwordForm, setPasswordForm] = useState({
+    password: ''
+  });
+
+  const onBanUser = e => {
+    e.preventDefault();
+    banUser(user._id , token, passwordForm.password);
+  }
+
   return (
-    <div className="user-item">
-      <div className="user-item__infos">
-        <span className="user-item__username">{user.username}</span>
-        <span className="user-item__email">{user.email}</span>
-        <span className="user-item__role">{user.role}</span>
-        <span className="user-item__status">{user.status}</span>
+    <Fragment>
+      <div className="user-item">
+        <div className="user-item__infos">
+          <span className="user-item__username">{user.username}</span>
+          <span className="user-item__email">{user.email}</span>
+          <span className="user-item__role">{user.role}</span>
+          <span className="user-item__status">{user.status}</span>
+        </div>
+        <div className="user-item__actions">
+          <Button type={'button'}>Change Role</Button>
+          <Button type={'button'} onClick={() => setOpenPassWordForm(true)}>Ban</Button>
+        </div>
       </div>
-      <div className="user-item__actions">
-        <Button type={'button'}>Change Role</Button>
-        <Button type={'button'}>Ban</Button>
-      </div>
-    </div>
+      {
+        openPassWordForm && (
+          <PasswordForm 
+            onSubmit={onBanUser}
+            onCancel={() => setOpenPassWordForm(false)}
+            onChange={e => setPasswordForm({...passwordForm, password: e.target.value})}
+            value={passwordForm.password}
+          />
+        )
+      }
+    </Fragment>
   )
 }
 
