d99fd16c82a95ca6956b2704410b5ef72efa6fc7
Julien Blondeau <julienblondeau@live.fr>
2020-05-23 17:55:45 +0200

    prepare deploy heroku

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const {createDefaultAdmin} = require('./createDefaultAdminUser');
 const HttpError = require('./models/http-error');
@@ -37,5 +38,13 @@ app.use((error, req, res, next) => {
   res.status(error.code || 500);
   res.json({message: error.message || 'An unknown error occured!'});
 });
+
+//Server static assets in production
+if(process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 module.exports = app;
