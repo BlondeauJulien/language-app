43ca165e9085bbdfa93b77da51e4698bd0497eef
Julien Blondeau <julienblondeau@live.fr>
2020-05-23 10:50:04 +0200

    responsive pagination

--- client/src/shared/components/UIElements/Pagination.js
+++ client/src/shared/components/UIElements/Pagination.js
@@ -5,9 +5,10 @@ import './Pagination.css';
 const Pagination = props => {
   const { postsPerPage, currentPage, totalItems, paginate } = props;
   const pageNumbers = [];
-  for (let i = 1; i <= Math.ceil(totalItems / postsPerPage); i++) {
-
-    pageNumbers.push(i);
+  for (let i = currentPage - 3; i <= currentPage + 3; i++) {
+    if(i > 0 && i <= Math.ceil(totalItems / postsPerPage)) {
+      pageNumbers.push(i);
+    }
   }
 
   return (
@@ -23,7 +24,7 @@ const Pagination = props => {
             </li>
           </a>
         ))}
-        <a onClick={() => paginate(pageNumbers.length)} href="#main-cont">
+        <a onClick={() => paginate(Math.ceil(totalItems / postsPerPage))} href="#main-cont">
           <li className='pagination-item'><i className="fas fa-step-forward"></i></li>
         </a>
       </ul>
