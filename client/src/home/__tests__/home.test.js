938eb8a97d56b1ce7b723c0adb491a73d7f970a0
Julien Blondeau <julienblondeau@live.fr>
2020-06-14 11:53:26 +0200

    basic home test

--- client/src/home/__tests__/home.test.js
+++ client/src/home/__tests__/home.test.js
@@ -3,7 +3,7 @@ import { MemoryRouter} from 'react-router-dom';
 import App from '../../App';
 import CourseState from '../../context/course/CourseState';
 import AuthState from '../../context/auth/AuthState';
-import {render} from '@testing-library/react';
+import {render, fireEvent} from '@testing-library/react';
 
 const renderHome = () => {
   return render(
@@ -18,10 +18,24 @@ const renderHome = () => {
 }
 
 describe("home component", () => {
-  it('Quick search input should start empty', () => { 
+  it('Quick search input should start empty and change on input', () => { 
     const { getByPlaceholderText } = renderHome();
     
     const quickSearchInput = getByPlaceholderText(/e\.g/i);
     expect(quickSearchInput.value).toBe('');
+
+    fireEvent.change(quickSearchInput, {target: {value: 'test text' } } );
+    expect(quickSearchInput.value).toBe('test text');
   })
+
+  it('222', () => { 
+    const { getByPlaceholderText } = renderHome();
+    
+    const quickSearchInput = getByPlaceholderText(/e\.g/i);
+    expect(quickSearchInput.value).toBe('');
+
+  })
+
+  
+
 })
